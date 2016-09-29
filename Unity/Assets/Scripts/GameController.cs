78c8a64c1ec3ecf48b44809ac95b550560e137ae
Yoon Kim <ykim.imlab@gmail.com>
2016-09-29 21:58:26 +0900

    Added GameController

---
+++ Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿//
+//  GameController.cs
+//  OculusLeap
+//
+//  Created by merongworld on 09/23/2016.
+//  Copyright (c) 2016 Merong World. All rights reserved.
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
