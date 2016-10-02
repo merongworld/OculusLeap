33efe94227bade8acd03ed81507b2e94f98216b5
primeiros <spicyblue_@naver.com>
2016-10-02 22:06:38 +0900

    ADD Leapmotion Widget Folder / with State Script

---
+++ Unity/Assets/ArmUI/Scripts/ArmHUDMGR.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Leap;
+using UnityEngine.UI;
+
+namespace WidgetShowcase
+{
+    public class ArmHUDMGR : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
