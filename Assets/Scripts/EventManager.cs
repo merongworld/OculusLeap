5b5e6e89a2a7fa235dfe12310310c96aabdfe07f
seokjae <spicyblue_@naver.com>
2016-10-29 03:10:51 +0900

    Add BlockMGR
    FYI

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class EventManager : MonoBehaviour {
+
+    [Header("References")]
+    public GameObject prefab;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update() {
+    }
+
+    public void onClickEvent() {    
+        GameObject obj = Instantiate(prefab, new Vector3(0, 0.3f, 0.3f), Quaternion.identity) as GameObject;
+    }
+}
