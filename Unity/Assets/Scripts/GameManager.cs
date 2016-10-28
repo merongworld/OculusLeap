deaf3d6e869d9af4031aec7c4f21b82b907d8f69
Yoon Kim <ykim.imlab@gmail.com>
2016-10-28 20:52:29 +0900

    Added GameManager

---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿//
+//  GameManager.cs
+//  OculusLeap
+//
+//  Created by merongworld on 10/28/2016.
+//  Copyright (c) 2016 Merong World. All rights reserved.
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+}
