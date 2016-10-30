d84797818c6eae2f781c46dca0bc0beb3118e82f
seokjae <spicyblue_@naver.com>
2016-10-30 20:36:44 +0900

    Edit BlockMgr, Toggler
    Complete move, picking

--- Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
+++ Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
@@ -2,23 +2,45 @@
 using UnityEngine.UI;
 using System.Collections;
 
-namespace Leap.Unity.InputModule {
-  public class ToggleToggler : MonoBehaviour {
-    public Text text;
-    public UnityEngine.UI.Image image;
-    public Color OnColor;
-    public Color OffColor;
+namespace Leap.Unity.InputModule
+{
+    public class ToggleToggler : MonoBehaviour
+    {
+        public Text text;
+        public UnityEngine.UI.Image image;
+        public Color OnColor;
+        public Color OffColor;
 
-    public void SetToggle(Toggle toggle) {
-      if (toggle.isOn) {
-        text.text = "On";
-        text.color = Color.white;
-        image.color = OnColor;
-      } else {
-        text.text = "Off";
-        text.color = new Color(0.3f, 0.3f, 0.3f);
-        image.color = OffColor;
-      }
+        public void SetToggle(Toggle toggle)
+        {
+            if (toggle.isOn)
+            {
+                text.text = "On";
+                text.color = Color.white;
+                image.color = OnColor;
+            }
+            else
+            {
+                text.text = "Off";
+                text.color = new Color(0.3f, 0.3f, 0.3f);
+                image.color = OffColor;
+            }
+        }
+
+        public void CopySetToggle(Toggle toggle)
+        {
+            if (toggle.isOn)
+            {
+                text.text = "Copy on";
+                text.color = Color.white;
+                image.color = OnColor;
+            }
+            else
+            {
+                text.text = "Copy(T)";
+                text.color = new Color(0.3f, 0.3f, 0.3f);
+                image.color = OffColor;
+            }
+        }
     }
-  }
 }
--- Assets/Scripts/BlockMGR.cs
+++ Assets/Scripts/BlockMGR.cs
@@ -15,15 +15,6 @@ public enum GrabMode
 
 public class BlockMGR : MonoBehaviour
 {
-
-    List<GameObject> block_prefab;
-    List<GameObject> blocks;
-    GameObject hitObject;
-    GameObject childObj;
-
-    private int handcount;
-    private string handtext;
-
     [Header("Reference")]
     public CapsuleHand left_hand;
     public CapsuleHand right_hand;
@@ -34,10 +25,21 @@ public class BlockMGR : MonoBehaviour
     public GameObject toggle_panel;
     public Text mount_panel;
 
+    List<GameObject> block_prefab;
+    //List<GameObject> blocks;
+    GameObject hitObject;
+    GameObject cubeAlphaObj;
+    GameObject cubeCanvas;
+
+    Toggle blockToggle;
+
+    private int handcount;
+    private string handtext;
+
     private GrabMode grabMode;
 
     // Timer to track focus
-    public float timeToSelect = 2.0f;
+    public float timeToSelect;
     private float countdown;
 
     GameObject geometryTargetObj;
@@ -45,20 +47,21 @@ public class BlockMGR : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        grabMode = GrabMode.Empty;
-
-        block_panel.SetActive(false);
-        geometry_panel.SetActive(false);
-        toggle_panel.SetActive(false);
-
         block_prefab = new List<GameObject>();
         block_prefab.Add(Resources.Load("BookShelfBlock", typeof(GameObject)) as GameObject);
         block_prefab.Add(Resources.Load("DirtBlock", typeof(GameObject)) as GameObject);
         block_prefab.Add(Resources.Load("GrassBlock", typeof(GameObject)) as GameObject);
         block_prefab.Add(Resources.Load("WoolBlock", typeof(GameObject)) as GameObject);
 
-        blocks = new List<GameObject>();
+        block_panel.SetActive(false);
+        geometry_panel.SetActive(false);
         toggle_panel.SetActive(false);
+
+        grabMode = GrabMode.Empty;
+        /*blockToggle = null;
+
+        blocks = new List<GameObject>();
+        */
     }
 
     void Update()
@@ -69,85 +72,104 @@ public class BlockMGR : MonoBehaviour
 
         mount_panel.text = setMountPanel();
 
-        Vector3 upNormal = new Vector3(0, 1, 0);
-        if (Vector3.Dot(UnityVectorExtension.ToVector3(left_hand.GetLeapHand().PalmNormal), upNormal) > 0 && Vector3.Dot(UnityVectorExtension.ToVector3(right_hand.GetLeapHand().PalmNormal), upNormal) > 0 && right_hand.GetLeapHand().GrabStrength > 0.9)
+        if (cubeCanvas != null && geometryTargetObj != null)
         {
-            block_panel.SetActive(true);
-        }
-        else if (left_hand.GetLeapHand().GrabStrength > 0.9)
-        {
-            // geometry_panel.SetActive(true);
+            cubeCanvas.transform.rotation = Quaternion.identity;
+            cubeCanvas.transform.position = geometryTargetObj.transform.position;
+            cubeCanvas.transform.Translate(0, 0, -0.05f);
         }
 
-        if (left_hand.GetLeapHand().GrabStrength > 0.95 && right_hand.GetLeapHand().GrabStrength > 0.95) grabMode = GrabMode.Both;
-        else if (left_hand.GetLeapHand().GrabStrength > 0.95) grabMode = GrabMode.LeftOnly;
-        else if (right_hand.GetLeapHand().GrabStrength > 0.95) grabMode = GrabMode.RightOnly;
-        else grabMode = GrabMode.Empty;
+        if (left_hand.isActiveAndEnabled && right_hand.isActiveAndEnabled)
+        {
+            if (left_hand.GetLeapHand().GrabStrength > 0.99 && right_hand.GetLeapHand().GrabStrength > 0.99) grabMode = GrabMode.Both;
+            else if (left_hand.GetLeapHand().GrabStrength > 0.99) grabMode = GrabMode.LeftOnly;
+            else if (right_hand.GetLeapHand().GrabStrength > 0.99) grabMode = GrabMode.RightOnly;
+            else grabMode = GrabMode.Empty;
+        }
 
         switch (grabMode)
         {
             case GrabMode.Empty:
-                if (Physics.Raycast(ray, out hit))
+                if (Physics.Raycast(ray, out hit)) // Hit
                 {
-                    if (hitObject != hit.collider.gameObject && hitObject != null)
+                    if (hitObject == hit.collider.gameObject) // 같은 블럭
                     {
-                        hitObject.transform.localScale = new Vector3(1f, 1f, 1f);
-                        childObj.SetActive(false);
-                        childObj = null;
-                    }
-                    hitObject = hit.collider.gameObject;
-                    hitObject.transform.localScale = new Vector3(1f, 1f, 1f);
+                        if (grabMode == GrabMode.Empty)
+                            hitObject.transform.localScale = new Vector3(1f, 1f, 1f);
 
-                    childObj = hitObject.transform.GetChild(6).gameObject;
-                    if (childObj.name == "Cube")
-                    {
-                        if (!childObj.activeSelf)
+                        if (!cubeAlphaObj.activeSelf)
                         {
                             countdown = timeToSelect;
-                            childObj.SetActive(true);
+                            cubeAlphaObj.SetActive(true);
                         }
                         else
                         {
                             countdown -= Time.deltaTime;
-
                             if (countdown < 0.0f)
                             {
-                                toggle_panel.SetActive(true);
-                                toggle_panel.transform.position = new Vector3(0, 0.3f, 0.45f);
-                                toggle_panel.transform.parent = hitObject.transform;
-
-                                geometry_panel.SetActive(false);
-
-                                GameObject toggleObj = toggle_panel.transform.GetChild(0).GetChild(0).gameObject;
-                                Toggle toggle = toggleObj.GetComponent<Toggle>();
-                                toggle.isOn = false;
-
+                                cubeCanvas.SetActive(true);
                                 countdown = timeToSelect;
                             }
                         }
                     }
+                    else if (hitObject != null) // 다른 블럭
+                    {
+                        if (blockToggle.isOn)
+                        {
+                            //nothing
+                        }
+                        else
+                        {
+                            cubeAlphaObj.SetActive(false);
+                            cubeCanvas.SetActive(false);
+                            cubeAlphaObj = null;
+                            cubeCanvas = null;
+                            blockToggle = null;
+                            hitObject = hit.collider.gameObject;
+                            cubeAlphaObj = hitObject.transform.GetChild(6).gameObject;
+                            cubeCanvas = hitObject.transform.GetChild(7).gameObject;
+                            blockToggle = cubeCanvas.transform.GetChild(0).GetChild(0).GetComponent<Toggle>();
+                        }
+                    }
+                    else // null 블럭
+                    {
+                        hitObject = hit.collider.gameObject;
+                        cubeAlphaObj = hitObject.transform.GetChild(6).gameObject;
+                        cubeCanvas = hitObject.transform.GetChild(7).gameObject;
+                        blockToggle = cubeCanvas.transform.GetChild(0).GetChild(0).GetComponent<Toggle>();
+                    }
                 }
-                else if (hitObject != null)
+                else // no Hit
                 {
                     hitObject.transform.localScale = new Vector3(1f, 1f, 1f);
-                    childObj = hitObject.transform.GetChild(6).gameObject;
-                    childObj.SetActive(false);
-                    hitObject = null;
-
-                    toggle_panel.SetActive(false);
-                    toggle_panel.transform.parent = GameObject.Find("Canvas").transform;
-                    countdown = timeToSelect;
+                    if (hitObject != null) // previous hit block exist
+                    {
+                        if (blockToggle.isOn)
+                        {
+                            //nothing
+                        }
+                        else
+                        {
+                            //hitObject = null, effect = false&null
+                            hitObject = null;
+                            cubeAlphaObj.SetActive(false);
+                            cubeCanvas.SetActive(false);
+                            cubeAlphaObj = null;
+                            cubeCanvas = null;
+                        }
+                    }
+                    else
+                    {
+                    }
                 }
-
                 break;
 
             case GrabMode.LeftOnly:
                 Debug.Log("left");
-                if (Vector3.Distance(hitObject.gameObject.transform.position, UnityVectorExtension.ToVector3(left_hand.GetLeapHand().PalmPosition)) < 0.11f)
+                if (Vector3.Distance(hitObject.gameObject.transform.position, UnityVectorExtension.ToVector3(left_hand.GetLeapHand().Fingers[0].TipPosition)) < 0.15f && !blockToggle.isOn)
                 {
                     hitObject.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-                    Vector3 newPosition = UnityVectorExtension.ToVector3(
-                        left_hand.GetLeapHand().PalmPosition) + UnityVectorExtension.ToVector3(left_hand.GetLeapHand().PalmNormal * 0.03f);
+                    Vector3 newPosition = UnityVectorExtension.ToVector3(left_hand.GetLeapHand().Fingers[0].TipPosition);
 
                     hitObject.transform.position = new Vector3(truncate(newPosition.x), truncate(newPosition.y), truncate(newPosition.z));
                 }
@@ -155,77 +177,76 @@ public class BlockMGR : MonoBehaviour
 
             case GrabMode.RightOnly:
                 Debug.Log("right");
-                if (Vector3.Distance(hitObject.gameObject.transform.position, UnityVectorExtension.ToVector3(right_hand.GetLeapHand().PalmPosition)) < 0.11f)
+                if (Vector3.Distance(hitObject.gameObject.transform.position, UnityVectorExtension.ToVector3(right_hand.GetLeapHand().Fingers[0].TipPosition)) < 0.15f && !blockToggle.isOn)
                 {
                     hitObject.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-                    Vector3 newPosition = UnityVectorExtension.ToVector3(
-                        right_hand.GetLeapHand().PalmPosition) + UnityVectorExtension.ToVector3(right_hand.GetLeapHand().PalmNormal * 0.03f);
+                    Vector3 newPosition = UnityVectorExtension.ToVector3(right_hand.GetLeapHand().Fingers[0].TipPosition);
 
                     hitObject.transform.position = new Vector3(truncate(newPosition.x), truncate(newPosition.y), truncate(newPosition.z));
                 }
                 break;
 
             case GrabMode.Both:
+                if (Vector3.Dot(UnityVectorExtension.ToVector3(left_hand.GetLeapHand().PalmNormal), Vector3.up) > 0 && Vector3.Dot(UnityVectorExtension.ToVector3(right_hand.GetLeapHand().PalmNormal), Vector3.up) > 0)
+                {
+                    block_panel.SetActive(true);
+                }
                 //float palm_distance = Vector3.Distance(UnityVectorExtension.ToVector3(left_hand.GetLeapHand().PalmPosition), UnityVectorExtension.ToVector3(right_hand.GetLeapHand().PalmPosition));
-                //hitObject.transform.localScale = new Vector3(truncate(palm_distance), truncate(palm_distance), truncate(palm_distance));             
+                //hitObject.transform.localScale = new Vector3(truncate(palm_distance), truncate(palm_distance), truncate(palm_distance));
                 break;
         }
     }
 
     public void onClickEvent(int listnum)
-    {
-        // toggle_panel.SetActive(true);
-        GameObject obj = Instantiate(block_prefab[listnum], new Vector3(0, 0.3f, 0.5f), Quaternion.identity) as GameObject;
-        // GameObject toggleobj = Instantiate(GameObject.Find("Toggle Canvas"), new Vector3(0, 0.3f, 0.45f), Quaternion.identity) as GameObject;
-        // toggleobj.transform.parent = obj.transform;
+    { 
+        toggle_panel.SetActive(true);
+        GameObject blockobj = Instantiate(block_prefab[listnum], new Vector3(0, block_panel.transform.position.y - 0.2f, block_panel.transform.position.z - 0.1f), Quaternion.identity) as GameObject;
+        GameObject toggleobj = Instantiate(GameObject.Find("Toggle Canvas"), new Vector3(0, block_panel.transform.position.y - 0.2f, block_panel.transform.position.y - 0.15f), Quaternion.identity) as GameObject;
+        toggle_panel.SetActive(false);
 
-        blocks.Add(obj);
+        toggleobj.transform.parent = blockobj.transform;
+        toggleobj.SetActive(false);
     }
 
     public void onToggleButtonClick()
     {
-        geometryTargetObj = hitObject;
-        geometry_panel.SetActive(true);
+        geometryTargetObj = cubeCanvas.transform.parent.gameObject;
+        if (blockToggle.isOn)
+            geometry_panel.SetActive(true);
+        else
+        {
+            geometryTargetObj = null;
+            geometry_panel.SetActive(false);
+        }
     }
 
     public void onXButtonClick()
     {
-        Vector3 rotation = geometryTargetObj.transform.rotation.eulerAngles;
-        int newAngle = (int)rotation.x + 90;
-
-        if (newAngle >= 360) newAngle -= 360;
-        geometryTargetObj.transform.rotation = Quaternion.Euler(new Vector3(newAngle, rotation.y, rotation.z));
+        geometryTargetObj.transform.Rotate(90, 0, 0);
     }
 
     public void onYButtonClick()
     {
-        Vector3 rotation = geometryTargetObj.transform.rotation.eulerAngles;
-        int newAngle = (int)rotation.y + 90;
-
-        if (newAngle >= 360) newAngle -= 360;
-        geometryTargetObj.transform.rotation = Quaternion.Euler(new Vector3(rotation.x, newAngle, rotation.z));
+        geometryTargetObj.transform.Rotate(0, 90, 0);
     }
 
     public void onZButtonClick()
     {
-        Vector3 rotation = geometryTargetObj.transform.rotation.eulerAngles;
-        int newAngle = (int)rotation.z + 90;
-
-        if (newAngle >= 360) newAngle -= 360;
-        geometryTargetObj.transform.rotation = Quaternion.Euler(new Vector3(rotation.x, rotation.y, newAngle));
+        geometryTargetObj.transform.Rotate(0, 0, 90);
     }
 
     public void onDeleteButtonClick()
     {
-        blocks.Remove(geometryTargetObj);
         Destroy(geometryTargetObj);
-
+        geometryTargetObj.SetActive(false);
         geometryTargetObj = null;
         geometry_panel.SetActive(false);
     }
 
     public void onCompleteButtonClick()
     {
+        blockToggle.isOn = false;
+        geometryTargetObj.SetActive(false);
         geometryTargetObj = null;
         geometry_panel.SetActive(false);
     }
@@ -263,3 +284,4 @@ public class BlockMGR : MonoBehaviour
         return "Hand(" + handcount + ") / " + handtext;
     }
 }
+
