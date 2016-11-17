1b62c38e3c1401d69eb073fa46b8be565cd925c2
Yoon Kim <ykim.imlab@gmail.com>
2016-11-18 07:56:02 +0900

    Edited StateController

--- Assets/Scripts/StateController.cs
+++ Assets/Scripts/StateController.cs
@@ -7,50 +7,89 @@
 //
 
 using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
+using Leap;
+
+public enum HandIconState
+{
+    None,
+    Valid,
+    Invalid
+}
 
 // To keep track of current state
 public struct State
 {
-    public DeviceState deviceState;
+    public DeviceState device;
+    public HandState hand;
+    public HandIconState leftHandIcon;
+    public HandIconState rightHandIcon;
 }
 
 public class StateController : MonoBehaviour
 {
-    private State current;
-
+    private State currentState;
     private LeapController leapController;
-    private GameObject leapMotionIcon;
 
+    private GameObject leapMotionIcon;
     private Sprite leapMotionBlackSprite;
     private Sprite leapMotionGreenSprite;
+
+    private GameObject leftHandIcon;
+    private Sprite leftHandGraySprite;
+    private Sprite leftHandRedSprite;
+    private Sprite leftHandGreenSprite;
+
+    private GameObject rightHandIcon;
+    private Sprite rightHandGraySprite;
+    private Sprite rightHandRedSprite;
+    private Sprite rightHandGreenSprite;
 
     // Use this for initialization
     void Start()
     {
-        current.deviceState = DeviceState.Disconnected;
+        currentState.device = DeviceState.Disconnected;
+        currentState.hand = HandState.None;
+        currentState.leftHandIcon = HandIconState.None;
+        currentState.rightHandIcon = HandIconState.None;
 
         leapController = FindObjectOfType<LeapController>() as LeapController;
-        leapMotionIcon = GameObject.Find("LeapMotionIcon");
 
+        leapMotionIcon = GameObject.Find("LeapMotionIcon");
         leapMotionBlackSprite = Resources.Load<Sprite>("leap_motion_black");
         leapMotionGreenSprite = Resources.Load<Sprite>("leap_motion_green");
+
+        leftHandIcon = GameObject.Find("LeftHandIcon");
+        leftHandIcon.SetActive(false);
+
+        rightHandIcon = GameObject.Find("RightHandIcon");
+        rightHandIcon.SetActive(false);
+
+        leftHandGraySprite = Resources.Load<Sprite>("left_hand_gray");
+        leftHandRedSprite = Resources.Load<Sprite>("left_hand_red");
+        leftHandGreenSprite = Resources.Load<Sprite>("left_hand_green");
+
+        rightHandGraySprite = Resources.Load<Sprite>("right_hand_gray");
+        rightHandRedSprite = Resources.Load<Sprite>("right_hand_red");
+        rightHandGreenSprite = Resources.Load<Sprite>("right_hand_green");
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateLeapMotionIcon();
+        UpdateHandIcons();
+        SetHandIconsActive(currentState.device);
     }
 
     void UpdateLeapMotionIcon()
     {
         DeviceState deviceState = leapController.DeviceState;
-        if (deviceState == current.deviceState) { return; }
+        if (deviceState == currentState.device) { return; }
 
-        current.deviceState = deviceState;
-        Image image = leapMotionIcon.GetComponent<Image>();
+        currentState.device = deviceState;
+        UnityEngine.UI.Image image =
+            leapMotionIcon.GetComponent<UnityEngine.UI.Image>();
 
         switch (deviceState)
         {
@@ -62,5 +101,117 @@ public class StateController : MonoBehaviour
                 image.sprite = leapMotionGreenSprite;
                 break;
         }
+    }
+
+    void UpdateHandIcons()
+    {
+        HandState handState = leapController.HandState;
+        Hand leftHand = leapController.LeftHand;
+        Hand rightHand = leapController.RightHand;
+
+        UnityEngine.UI.Image leftHandImage =
+            leftHandIcon.GetComponent<UnityEngine.UI.Image>();
+
+        UnityEngine.UI.Image rightHandImage =
+            rightHandIcon.GetComponent<UnityEngine.UI.Image>();
+
+        HandIconState leftHandIconState = GetHandIconState(
+            currentState.device, handState, leftHand);
+
+        HandIconState rightHandIconState = GetHandIconState(
+            currentState.device, handState, rightHand);
+
+        if (leftHandIconState != currentState.leftHandIcon)
+        {
+            currentState.leftHandIcon = leftHandIconState;
+            UnityEngine.UI.Image image =
+                leftHandIcon.GetComponent<UnityEngine.UI.Image>();
+
+            switch (currentState.leftHandIcon)
+            {
+                case HandIconState.None:
+                    image.sprite = leftHandGraySprite;
+                    break;
+
+                case HandIconState.Valid:
+                    image.sprite = leftHandGreenSprite;
+                    break;
+
+                case HandIconState.Invalid:
+                    image.sprite = leftHandRedSprite;
+                    break;
+            }
+        }
+        else if (rightHandIconState != currentState.rightHandIcon)
+        {
+            currentState.rightHandIcon = rightHandIconState;
+            UnityEngine.UI.Image image =
+                rightHandIcon.GetComponent<UnityEngine.UI.Image>();
+
+            switch (currentState.rightHandIcon)
+            {
+                case HandIconState.None:
+                    image.sprite = rightHandGraySprite;
+                    break;
+
+                case HandIconState.Valid:
+                    image.sprite = rightHandGreenSprite;
+                    break;
+
+                case HandIconState.Invalid:
+                    image.sprite = rightHandRedSprite;
+                    break;
+            }
+        }
+    }
+
+    void SetHandIconsActive(DeviceState deviceState)
+    {
+        switch (deviceState)
+        {
+            case DeviceState.Disconnected:
+                leftHandIcon.SetActive(false);
+                rightHandIcon.SetActive(false);
+                break;
+
+            case DeviceState.Connected:
+                leftHandIcon.SetActive(true);
+                rightHandIcon.SetActive(true);
+                break;
+        }
+    }
+
+    HandIconState GetHandIconState(
+        DeviceState deviceState, HandState handState, Hand hand)
+    {
+        if (deviceState == DeviceState.Disconnected)
+        {
+            return HandIconState.None;
+        }
+
+        switch (handState)
+        {
+            case HandState.None:
+                return HandIconState.None;
+
+            case HandState.Left:
+                if (hand == null) { return HandIconState.None; }
+                else if (hand.IsLeft) { return HandIconState.Valid; }
+                break;
+
+            case HandState.Right:
+                if (hand == null) { return HandIconState.None; }
+                else if (!hand.IsLeft) { return HandIconState.Valid; }
+                break;
+
+            case HandState.Both:
+                return HandIconState.Valid;
+
+            case HandState.Invalid:
+                if (hand != null) { return HandIconState.Invalid; }
+                return HandIconState.None;
+        }
+
+        return HandIconState.Invalid;
     }
 }
