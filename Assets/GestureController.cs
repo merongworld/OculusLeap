450bc3614e2b9851121e943c5137d2d8f7579961
Yoon Kim <ykim.imlab@gmail.com>
2016-11-21 11:28:28 +0900

    Added GestureController

---
+++ Assets/GestureController.cs
@@ -0,0 +1,29 @@
+﻿//
+//  GestureController.cs
+//  OculusLeap
+//
+//  Created by merongworld on 11/21/2016.
+//  Copyright (c) 2016 Merong World. All rights reserved.
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class GestureController : MonoBehaviour
+{
+    GameController gameController;
+    LeapController leapController;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+        leapController = FindObjectOfType<LeapController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
