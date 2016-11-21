ea457da46ed702ec1b05fe9c89528035b2b6630d
Yoon Kim <ykim.imlab@gmail.com>
2016-11-21 10:56:31 +0900

    Added GameController

---
+++ Assets/GameController.cs
@@ -0,0 +1,36 @@
+﻿//
+//  GameController.cs
+//  OculusLeap
+//
+//  Created by merongworld on 11/21/2016.
+//  Copyright (c) 2016 Merong World. All rights reserved.
+//
+
+using UnityEngine;
+using System.Collections;
+
+public enum MenuState
+{
+    None,
+    Main,
+    Add,
+    Edit,
+    Settings
+}
+
+public class GameController : MonoBehaviour
+{
+    MenuState menuState;
+
+    // Use this for initialization
+    void Start()
+    {
+        menuState = MenuState.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
